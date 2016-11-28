a490ca00870007ec85d0bc96a68816dde7aed72d
FabianB <FabianB@DESKTOP-GD3MJGQ>
2016-11-28 09:05:11 -0600

    Add project files.

---
+++ BORRAR_GUIT_TEST/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BORRAR_GUIT_TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HOLA MUNDO!");
+            Console.ReadKey();
+        }
+    }
+}
